c82d503c8c04a4430fbc5b889dfbe95b9cc12a04
Sankofa-Digital-XCIV <thosontula@gmail.com>
2026-07-28 11:57:19 +0200

    test: add Cypress configuration

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  video: false,
+  screenshotsFolder: 'tmp/cypress-screenshots',
+  screenshotOnRunFailure: true,
+  viewportWidth: 1366,
+  viewportHeight: 768,
+});
